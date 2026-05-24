f1dc31e24aa139c552727f3a922748adb9cce975
santhosh teja piridi <santhosh.piridi123@gmail.com>
2026-05-24 19:36:09 +0530

    fix: add headless chrome options for CI

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -5,5 +5,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return driver
